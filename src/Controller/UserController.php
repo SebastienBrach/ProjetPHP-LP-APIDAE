38182220bf47b6a2edba5bc75ac7b7d3b4365937
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-13 16:33:56 +0100

    Création de my account

---
+++ src/Controller/UserController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+
+
+class UserController extends AbstractController
+{
+    /**
+     * @Route("/account", name="account_user")
+     */
+    public function checkAccount(): Response {
+        $repository = $this->getDoctrine()->getRepository(User::class);
+        $account = $repository->findAll();
+        return $this->render('user/account.html.twig', [
+            'account' => $account,
+        ]);
+    }
+}
