722e564d8b1d32f045832d32f392e6d3579d4dc9
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-13 15:47:23 +0100

    navbar OK

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -25,6 +25,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
     use TargetPathTrait;
 
     public const LOGIN_ROUTE = 'app_login';
+    public const HOME = 'home';
+
 
     private $entityManager;
     private $urlGenerator;
@@ -98,7 +100,7 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator implements P
 
         // For example : return new RedirectResponse($this->urlGenerator->generate('some_route'));
         // throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
-        return new RedirectResponse($this->router->generate('home'));
+        return new RedirectResponse($this->router->generate(self::HOME));
     }
 
     protected function getLoginUrl()
