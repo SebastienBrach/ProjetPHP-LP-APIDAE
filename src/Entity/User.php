2a78e0898d93099b973356ad60bde2b4b25b959f
Brach Sebastien <sebastien.brach@etu.umontpellier.fr>
2020-12-13 16:28:59 +0100

    Niquel

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -39,7 +39,7 @@ class User implements UserInterface
     private $email;
 
     /**
-     * @ORM\Column(type="array")
+     * @ORM\Column(type="json")
      */
     private $roles = [];
 
@@ -107,7 +107,7 @@ class User implements UserInterface
     {
         $roles = $this->roles;
         // guarantee every user at least has ROLE_USER
-        $roles[] = 'ROLE_ADMIN';
+        $roles[] = 'ROLE_USER';
 
         return array_unique($roles);
     }
